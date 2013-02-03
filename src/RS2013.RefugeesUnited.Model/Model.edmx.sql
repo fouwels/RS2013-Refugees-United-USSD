b8ecc51e89d8e0025ea9405056810cef4a266cdb
James Billingham <james@jamesbillingham.com>
2013-02-03 20:37:35 +0000

    Database generation & mappings

---
+++ src/RS2013.RefugeesUnited.Model/Model.edmx.sql
@@ -0,0 +1,127 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, and Azure
+-- --------------------------------------------------
+-- Date Created: 02/03/2013 12:36:52
+-- Generated from EDMX file: Z:\Projects\RefUnited\src\RS2013.RefugeesUnited.Model\Model.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [RefugeesUnited];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_DeviceUser]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Users] DROP CONSTRAINT [FK_DeviceUser];
+GO
+IF OBJECT_ID(N'[dbo].[FK_UserSession]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Sessions] DROP CONSTRAINT [FK_UserSession];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Devices]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Devices];
+GO
+IF OBJECT_ID(N'[dbo].[Users]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Users];
+GO
+IF OBJECT_ID(N'[dbo].[Sessions]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Sessions];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Devices'
+CREATE TABLE [dbo].[Devices] (
+    [Id] bigint IDENTITY(1,1) NOT NULL,
+    [Number] nvarchar(max)  NOT NULL,
+    [Blacklisted] bit  NOT NULL
+);
+GO
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Id] bigint IDENTITY(1,1) NOT NULL,
+    [ApiId] nvarchar(max)  NOT NULL,
+    [Device_Id] bigint  NULL
+);
+GO
+
+-- Creating table 'Sessions'
+CREATE TABLE [dbo].[Sessions] (
+    [Id] bigint IDENTITY(1,1) NOT NULL,
+    [UssdId] nvarchar(max)  NOT NULL,
+    [StartTimestamp] datetime  NOT NULL,
+    [ActivityTimestamp] datetime  NOT NULL,
+    [User_Id] bigint  NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Devices'
+ALTER TABLE [dbo].[Devices]
+ADD CONSTRAINT [PK_Devices]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Sessions'
+ALTER TABLE [dbo].[Sessions]
+ADD CONSTRAINT [PK_Sessions]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [Device_Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [FK_DeviceUser]
+    FOREIGN KEY ([Device_Id])
+    REFERENCES [dbo].[Devices]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_DeviceUser'
+CREATE INDEX [IX_FK_DeviceUser]
+ON [dbo].[Users]
+    ([Device_Id]);
+GO
+
+-- Creating foreign key on [User_Id] in table 'Sessions'
+ALTER TABLE [dbo].[Sessions]
+ADD CONSTRAINT [FK_UserSession]
+    FOREIGN KEY ([User_Id])
+    REFERENCES [dbo].[Users]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserSession'
+CREATE INDEX [IX_FK_UserSession]
+ON [dbo].[Sessions]
+    ([User_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
