c527d75c14c779d182b02a67ab3dfa4a3d4696cc
James Billingham <james@jamesbillingham.com>
2013-02-03 20:46:05 +0000

    Session service

---
+++ src/RS2013.RefugeesUnited.Services/ISessionService.cs
@@ -0,0 +1,9 @@
+﻿using RS2013.RefugeesUnited.Model;
+
+namespace RS2013.RefugeesUnited.Services
+{
+	public interface ISessionService
+	{
+		Session RetrieveSession(string ussdSessionId);
+	}
+}
---
+++ src/RS2013.RefugeesUnited.Services/Impl/SessionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using RS2013.RefugeesUnited.Data;
+using RS2013.RefugeesUnited.Model;
+
+namespace RS2013.RefugeesUnited.Services.Impl
+{
+	public class SessionService : ISessionService
+	{
+		private ISessionRepository SessionRepository { get; set; }
+
+		public SessionService(ISessionRepository sessionRepository)
+		{
+			if (sessionRepository == null) throw new ArgumentNullException("sessionRepository");
+
+			SessionRepository = sessionRepository;
+		}
+
+		public Session RetrieveSession(string ussdSessionId)
+		{
+			return SessionRepository.GetAll().FirstOrDefault(s => s.UssdId == ussdSessionId);
+		}
+	}
+}
