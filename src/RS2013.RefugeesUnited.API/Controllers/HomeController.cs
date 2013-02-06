5057f949edaeee8ba4834887a946d98e354ac03d
James Billingham <james@jamesbillingham.com>
2013-02-06 19:45:48 +0000

    stuffness on homecontrolletron

--- src/RS2013.RefugeesUnited.API/Controllers/HomeController.cs
+++ src/RS2013.RefugeesUnited.API/Controllers/HomeController.cs
@@ -2,38 +2,36 @@
 using System.Web.Mvc;
 using RS2013.RefugeesUnited.Model;
 using RS2013.RefugeesUnited.Services;
-using RS2013.RefugeesUnited.Services.Impl;
-using Tam.Lib.Model;
 
 namespace RS2013.RefugeesUnited.API.Controllers
 {
 	public class HomeController : Controller
 	{
 		private ISessionService SessionService { get; set; }
+		private IRefugeesUnitedService RefugeesUnitedService { get; set; }
 
-		public HomeController(ISessionService sessionService)
+		public HomeController(ISessionService sessionService, IRefugeesUnitedService refugeesUnitedService)
 		{
 			SessionService = sessionService;
+			RefugeesUnitedService = refugeesUnitedService;
 		}
 
-		public ActionResult Index()
+		public async Task<ActionResult> Index()
 		{
-			IRefugeesUnitedService testService = new RefugeesUnitedService();
+			var testUsername = await RefugeesUnitedService.GenerateUsername("Kaelan", "Fouwels");
+			var testUserExists = await RefugeesUnitedService.UserExists("kaelan.fouwels");
+			var testLogin = await RefugeesUnitedService.Login(null, "kaelan.fouwels", "8740");
+			var testLogout = await RefugeesUnitedService.Logout("kaelanb.fouwels");
 
-			//var testUsername = testService.GenerateUsername("Kaelan", "Fouwels");
-			//var testUserExists = testService.UserExists("kaelan.fouwels");
-			//var testLogin = testService.Login("kaelan.fouwels", "8740");
-			//var testLogout = testService.Logout("kaelanb.fouwels");
+			var testProfile = new RefUnitedProfile
+			{
+				givenName = "kaelan",
+				surName = "fouwels",
+				otherInformation = "I like trains",
+				lastSighting = "Test"
+			};
 
-			//RefUnitedProfile testProfile = new RefUnitedProfile
-			//	{
-			//		givenName = "kaelan",
-			//		surName = "fouwels",
-			//		otherInformation = "I like trains",
-			//		lastSighting = "Test"
-			//	};
-
-			//var testSearch = testService.Search(testProfile);
+			var testSearch = await RefugeesUnitedService.Search(testProfile);
 
 			return Content("");
 		}
