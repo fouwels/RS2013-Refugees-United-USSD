51def9d2ccf560754b1554947aa502246e1320a2
James Billingham <james@jamesbillingham.com>
2013-02-07 01:56:26 +0000

    Session default vals

---
+++ src/RS2013.RefugeesUnited.Model/Session.Partial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RS2013.RefugeesUnited.Model
+{
+	public partial class Session
+	{
+		public Session()
+		{
+			ActivityTimestamp = DateTime.Now;
+			StartTimestamp = DateTime.Now;
+			State = SessionState.AuthenticationOptions;
+		}
+	}
+}
